925cc3cd1445893565075e6596dd614e8e3b5b3e
Vanessa Graber <vanessa.graber@mcgill.ca>
2019-06-04 15:13:20 -0400

    Initial commit

---
+++ ns_eos/equilibrium_comp.py
@@ -0,0 +1,29 @@
+"""Calculation of chemical potentials based on baryon conservation, charge neutrality, beta equilibrium
+    and muon production rate for a given set of Skyrme parameters as done in Chamel (2008)
+"""
+
+import numpy as np
+from scipy.optimize import newton
+from typing import Tuple
+
+# natural constants
+c = 2.9979246e23  # speed of light in fm/s
+hbar = 6.582120e-22  # hbar in MeV s
+m_u = 1.0364270e-44  # atomic mass unit in MeV s^2/fm^2
+m_mu = 0.113429 * m_u  # muon mass unit in MeV s^2/fm^2
+
+# constant related to the appearance of muon
+const = m_mu ** 2 * c ** 2 / (hbar ** 2 * (3 * np.pi ** 2) ** (2 / 3))
+
+
+def relation_nmu_ne(n_e: float) -> float:
+    """function relates the muon number density to a given electron number density;
+    calculated from the equality of the muon and electron chemical potentials"""
+
+    # for low electron number densities, no muons are present
+    if n_e < const ** (3 / 2):
+        return 0.0
+    # above a critical electron number density muons appear
+    else:
+        n_mu = (n_e ** (2 / 3) - const) ** (3 / 2)
+        return n_mu
---
+++ ns_eos/tests/test_equilibrium_comp.py
@@ -0,0 +1,20 @@
+import ns_eos.equilibrium_comp as ec
+
+
+def test_const():
+    """verifying the value of the constant affecting the muon apprearance"""
+    assert ec.const == 0.029956297154719348
+
+
+def test_relation_nmu_ne_01():
+    """verifying the relationship between the muon and electron density in absence of muons"""
+    n_e = 0.001
+    n_mu = ec.relation_nmu_ne(n_e)
+    assert n_mu == 0.0
+
+
+def test_relation_nmu_ne_02():
+    """verifying the relationship between the muon and electron density in presence of muons"""
+    n_e = 0.1
+    n_mu = ec.relation_nmu_ne(n_e)
+    assert n_mu == 0.0798860263555066
