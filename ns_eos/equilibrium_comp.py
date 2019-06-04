a13adced7956eb5bc9601ed21323bcddc6283116
Vanessa Graber <vanessa.graber@mcgill.ca>
2019-06-04 15:51:38 -0400

    Adding a class that deals with the EoS related quantities

--- ns_eos/equilibrium_comp.py
+++ ns_eos/equilibrium_comp.py
@@ -39,3 +39,61 @@ def relation_np_ne(n_e: float) -> float:
     # above the critical electron number density, muons contribute
     else:
         return relation_nmu_ne(n_e) + n_e
+
+
+class EquationOfState:
+    def __init__(
+        self,
+        t0=-2719.7,
+        t1=417.64,
+        t2=-66.687,
+        t3=15042.0,
+        x0=0.16154,
+        x1=-0.047986,
+        x2=0.027170,
+        x3=0.13611,
+        alpha=0.14416,
+    ):
+        """
+        :param t0: Skyrme parameter
+        :type t0: float
+        :param t1: Skyrme parameter
+        :type t1: float
+        :param t2: Skyrme parameter
+        :type t2: float
+        :param t3: Skyrme parameter
+        :type t3: float
+        :param x0: Skyrme parameter
+        :type x0: float
+        :param x1: Skyrme parameter
+        :type x1: float
+        :param x2: Skyrme parameter
+        :type x2: float
+        :param x3: Skyrme parameter
+        :type x3: float
+        :param alpha: Skyrme parameter
+        :type alpha: float
+        """
+
+        # Skyrme parameters
+        self.t0 = t0
+        self.t1 = t1
+        self.t2 = t2
+        self.t3 = t3
+        self.x0 = x0
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+        self.alpha = alpha
+
+        def _parameters_hamiltonian(self) -> Tuple[float, ...]:
+            """function calculates the parameters for the effective Skyrme Hamiltonian"""
+
+            B1 = (self.t0 / 2) * (1 + self.x0 / 2)
+            B2 = -(self.t0 / 2) * (self.x0 + 1 / 2)
+            B3 = (1 / 4) * (self.t1 * (1 + self.x1 / 2) + self.t2 * (1 + self.x2 / 2))
+            B4 = -(1 / 4) * (self.t1 * (self.x1 + 1 / 2) - self.t2 * (self.x2 + 1 / 2))
+            B5 = (self.t3 / 12) * (1 + self.x3 / 2)
+            B6 = -(self.t3 / 12) * (self.x3 + 1 / 2)
+
+            return B1, B2, B3, B4, B5, B6
