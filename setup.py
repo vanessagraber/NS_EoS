a86615c2b5c7e56e22723bbcec61331eedc02f19
Vanessa Graber <vanessa.graber@mcgill.ca>
2019-06-04 15:10:13 -0400

    Adding pytest-cov and pytest-runner to environment and setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="ns_eos",
-      packages=find_packages(),
-      # setup_requires=["pytest-runner"],
-      tests_require=["pytest"])
+setup(
+    name="ns_eos",
+    packages=find_packages(),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
