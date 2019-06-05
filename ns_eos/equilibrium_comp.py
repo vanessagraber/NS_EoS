eb30c1c87c747dcda7746b769b895623f2e10efd
Vanessa Graber <vanessa.graber@mcgill.ca>
2019-06-05 11:21:57 -0400

    Adding methods to class that allow calculating the electron number density for any given baryon density

--- ns_eos/equilibrium_comp.py
+++ ns_eos/equilibrium_comp.py
@@ -13,7 +13,7 @@ m_u = 1.0364270e-44  # atomic mass unit in MeV s^2/fm^2
 m_mu = 0.113429 * m_u  # muon mass unit in MeV s^2/fm^2
 
 # constant related to the appearance of muon
-const = m_mu ** 2 * c ** 2 / (hbar ** 2 * (3 * np.pi ** 2) ** (2 / 3))
+muon_eqn_const = m_mu ** 2 * c ** 2 / (hbar ** 2 * (3 * np.pi ** 2) ** (2 / 3))
 
 
 def relation_nmu_ne(n_e: float) -> float:
@@ -21,11 +21,11 @@ def relation_nmu_ne(n_e: float) -> float:
     calculated from the equality of the muon and electron chemical potentials"""
 
     # for low electron number densities, no muons are present
-    if n_e < const ** (3 / 2):
+    if n_e < muon_eqn_const ** (3 / 2):
         return 0.0
     # above a critical electron number density muons appear
     else:
-        n_mu = (n_e ** (2 / 3) - const) ** (3 / 2)
+        n_mu = (n_e ** (2 / 3) - muon_eqn_const) ** (3 / 2)
         return n_mu
 
 
@@ -34,7 +34,7 @@ def relation_np_ne(n_e: float) -> float:
     obtained from the charge neutrality condition"""
 
     # below first appearance of muons, electron and proton number density are equivalent
-    if n_e < const ** (3 / 2):
+    if n_e < muon_eqn_const ** (3 / 2):
         return n_e
     # above the critical electron number density, muons contribute
     else:
@@ -86,14 +86,55 @@ class EquationOfState:
         self.x3 = x3
         self.alpha = alpha
 
-        def _parameters_hamiltonian(self) -> Tuple[float, ...]:
-            """function calculates the parameters for the effective Skyrme Hamiltonian"""
+    def _parameters_hamiltonian(self) -> Tuple[float, ...]:
+        """function calculates the parameters for the effective Skyrme Hamiltonian"""
 
-            B1 = (self.t0 / 2) * (1 + self.x0 / 2)
-            B2 = -(self.t0 / 2) * (self.x0 + 1 / 2)
-            B3 = (1 / 4) * (self.t1 * (1 + self.x1 / 2) + self.t2 * (1 + self.x2 / 2))
-            B4 = -(1 / 4) * (self.t1 * (self.x1 + 1 / 2) - self.t2 * (self.x2 + 1 / 2))
-            B5 = (self.t3 / 12) * (1 + self.x3 / 2)
-            B6 = -(self.t3 / 12) * (self.x3 + 1 / 2)
+        B1 = (self.t0 / 2) * (1 + self.x0 / 2)
+        B2 = -(self.t0 / 2) * (self.x0 + 1 / 2)
+        B3 = (1 / 4) * (self.t1 * (1 + self.x1 / 2) + self.t2 * (1 + self.x2 / 2))
+        B4 = -(1 / 4) * (self.t1 * (self.x1 + 1 / 2) - self.t2 * (self.x2 + 1 / 2))
+        B5 = (self.t3 / 12) * (1 + self.x3 / 2)
+        B6 = -(self.t3 / 12) * (self.x3 + 1 / 2)
 
-            return B1, B2, B3, B4, B5, B6
+        return B1, B2, B3, B4, B5, B6
+
+    def relative_chem_pot(self, n_b: float, n_p: float) -> float:
+        """function determines the relative chemical potential of the protons and neutrons as a
+        function of baryon and proton number densities using the parameters of the Skyrme Hamiltonian"""
+
+        b_vector = self._parameters_hamiltonian()
+
+        del_mu = (
+            (3 * np.pi ** 2) ** (2 / 3)
+            * ((n_b - n_p) ** (2 / 3) - n_p ** (2 / 3))
+            * (hbar ** 2 / (2 * m_u) + b_vector[2] * n_b)
+            + 2 * (n_b - 2 * n_p) * (b_vector[1] + b_vector[5] * n_b ** self.alpha)
+            + (8 / 5)
+            * (3 * np.pi ** 2) ** (2 / 3)
+            * b_vector[3]
+            * ((n_b - n_p) ** (5 / 3) - n_p ** (5 / 3))
+        )
+        return del_mu
+
+    def func_to_minimize(self, n_e: float, n_b: float) -> float:
+        """based on beta equilibrium, the function equates the relative nucleon chemical potentials
+         with the electron chemical potential"""
+
+        n_p = relation_np_ne(n_e)
+        del_mu = self.relative_chem_pot(n_b, n_p)
+        mu_e = c * hbar * (3 * np.pi ** 2 * n_e) ** (1 / 3)
+
+        func_min = del_mu - mu_e
+
+        return func_min
+
+    def relation_ne_nb(self, n_b: np.ndarray) -> np.ndarray:
+        """solving the relation gives the electron number density for any given baryon number density"""
+
+        output = np.empty(n_b.shape) * np.nan
+
+        for i, x in enumerate(n_b):
+
+            output[i] = newton(self.func_to_minimize, 0.001, args=(x,))
+
+        return output
