51e163c119ed0188c54a95fa90e28ce4b57fae24
Vanessa Graber <vanessa.graber@mcgill.ca>
2019-07-26 18:07:46 -0400

    Adding the Skyrme model Skchi450 to the file generating test data.

--- examples/generating_eos_data.py
+++ examples/generating_eos_data.py
@@ -35,7 +35,7 @@ eos_NRAPR = ec.EquationOfState(
     x1=-0.0480,
     x2=0.0272,
     x3=0.1361,
-    alpha=0.1442,
+    sigma=0.1442,
 )
 
 n_n_NRAPR = eos_NRAPR.n_n(n_b)
@@ -93,7 +93,7 @@ eos_LNS = ec.EquationOfState(
     x1=0.6585,
     x2=-0.9538,
     x3=-0.0341,
-    alpha=1 / 6,
+    sigma=1 / 6,
 )
 
 n_n_LNS = eos_LNS.n_n(n_b)
@@ -151,7 +151,7 @@ eos_SLy4 = ec.EquationOfState(
     x1=-0.344,
     x2=-1,
     x3=1.354,
-    alpha=1 / 6,
+    sigma=1 / 6,
 )
 
 n_n_SLy4 = eos_SLy4.n_n(n_b)
@@ -209,7 +209,7 @@ eos_Sk = ec.EquationOfState(
     x1=-0.8,
     x2=0.0,
     x3=0.01,
-    alpha=0.35,
+    sigma=0.35,
 )
 
 n_n_Sk = eos_Sk.n_n(n_b)
@@ -260,14 +260,14 @@ df_Sk.to_csv("./examples/data/parameters_Ska35s20.txt", index=None, header=True)
 
 eos_SQMC = ec.EquationOfState(
     t0=-2429.1,
-    t1=371.0,
-    t2=-96.7,
-    t3=13773.6,
+    t1=370.97,
+    t2=-96.69,
+    t3=13773.43,
     x0=0.10,
     x1=0.0,
     x2=0.0,
     x3=0.0,
-    alpha=1 / 6,
+    sigma=1 / 6,
 )
 
 n_n_SQMC = eos_SQMC.n_n(n_b)
@@ -277,7 +277,7 @@ xi_n_SQMC = eos_SQMC.xi_n(n_b)
 xi_p_SQMC = eos_SQMC.xi_p(n_b)
 H_i_SQMC = eos_SQMC.H_parameters()
 
-print("H_i for SQMC: ", [round(H, 2) for H in H_i_SQMC])
+print("H_i for SQMC700: ", [round(H, 2) for H in H_i_SQMC])
 
 df_SQMC = pd.DataFrame(
     {
@@ -312,3 +312,64 @@ df_SQMC.columns = pd.MultiIndex.from_tuples(
     )
 )
 df_SQMC.to_csv("./examples/data/parameters_SQMC700.txt", index=None, header=True)
+
+
+# ---- Skchi450 - parameters are taken from Lim and Holt (2017) ---- #
+
+eos_Skchi450 = ec.EquationOfState(
+    t0=-1803.29,
+    t1=301.82,
+    t2=-273.28,
+    t3=12783.86,
+    t4=564.10,
+    x0=0.443,
+    x1=-0.3622,
+    x2=-0.4105,
+    x3=0.6545,
+    x4=-11.316,
+    sigma=1 / 3,
+    sigma_2=1,
+)
+
+n_n_Skchi450 = eos_Skchi450.n_n(n_b)
+n_p_Skchi450 = eos_Skchi450.n_p(n_b)
+lambda_Skchi450 = eos_Skchi450.lambda_L(n_b)
+xi_n_Skchi450 = eos_Skchi450.xi_n(n_b)
+xi_p_Skchi450 = eos_Skchi450.xi_p(n_b)
+H_i_Skchi450 = eos_Skchi450.H_parameters()
+
+print("H_i for Skchi450: ", [round(H, 2) for H in H_i_Skchi450])
+
+df_Skchi450 = pd.DataFrame(
+    {
+        "n_b": n_b,
+        "rho_b": rho_b,
+        "n_n": n_n_Skchi450,
+        "n_p": n_p_Skchi450,
+        "lambda": lambda_Skchi450,
+        "xi_n": xi_n_Skchi450,
+        "xi_p": xi_p_Skchi450,
+        "kappa": lambda_Skchi450 / xi_p_Skchi450,  # Ginzburg-Landau parameter
+        "R": xi_p_Skchi450 / xi_n_Skchi450,  # coherence length ratio
+        "epsilon": n_p_Skchi450 / n_n_Skchi450,  # asymmetry parameter
+    }
+)
+
+df_Skchi450.columns = pd.MultiIndex.from_tuples(
+    zip(
+        df_Skchi450.columns,
+        [
+            "[1/fm**3]",
+            "[g/cm**3]",
+            "[1/fm**3]",
+            "[1/fm**3]",
+            "[cm]",
+            "[cm]",
+            "[cm]",
+            "[]",
+            "[]",
+            "[]",
+        ],
+    )
+)
+df_Skchi450.to_csv("./examples/data/parameters_Skchi450.txt", index=None, header=True)
