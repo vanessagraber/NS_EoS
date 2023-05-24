a7ce4fd8f79ff60d1efc219ead3f9274143553eb
Vanessa Graber <graber@ice.csic.es>
2023-05-24 13:20:05 +0200

    Updating the tests to reflect the generalisation of the gap calculation to all fits from Ho et al. (2015).

--- ns_eos/tests/test_gap_parametrisation.py
+++ ns_eos/tests/test_gap_parametrisation.py
@@ -18,68 +18,92 @@ TOL = 1e-5
 
 @pytest.fixture()
 def test_case_1():
-    data = {"k_F_n": 0.5, "k_F_p": 0.0, "Delta_n": np.nan, "Delta_p": np.nan}
+    data = {"k_F": 0.5, "Delta": 0.5924535603715172}
 
     return data
 
 
 @pytest.fixture()
 def test_case_2():
-    data = {"k_F_n": 1.5, "k_F_p": 0.5, "Delta_n": 0.241528, "Delta_p": 0.824451}
+    data = {
+        "k_F_n_left": 0.0,
+        "k_F_n_right": 1.5,
+        "Delta_n_left": np.nan,
+        "Delta_n_right": np.nan,
+    }
 
     return data
 
 
 @pytest.fixture()
 def test_case_3():
-    data = {"k_F_n": 3.5, "k_F_p": 1.5, "Delta_n": np.nan, "Delta_p": np.nan}
+    data = {
+        "k_F_p_left": 0.0,
+        "k_F_p_right": 1.5,
+        "Delta_p_left": np.nan,
+        "Delta_p_right": np.nan,
+    }
 
     return data
 
 
-def test_gap_neutrons_01(test_case_1):
-    """verifying that the neutron energy gap vanishes at low k_F_n"""
-    Delta_n = gp.gap_neutrons(test_case_1["k_F_n"])
-    assert Delta_n is test_case_1["Delta_n"]
+@pytest.fixture()
+def test_case_4():
+    data = {
+        "k_F_n_left": 0.7,
+        "k_F_n_right": 3.5,
+        "Delta_n_left": np.nan,
+        "Delta_n_right": np.nan,
+    }
+
+    return data
 
 
-def test_gap_protons_01(test_case_1):
-    """verifying that the proton energy gap vanishes at low k_F_p"""
-    Delta_p = gp.gap_protons(test_case_1["k_F_p"])
-    assert Delta_p is test_case_1["Delta_p"]
+def test_gap_full(test_case_1):
+    """verifying that the energy gap is correctly calculated"""
+    Delta = gp.gap_full(test_case_1["k_F"], gp.neutron_triplet_TToa)
+    assert np.isclose(Delta, test_case_1["Delta"])
 
 
-def test_gap_neutrons_full(test_case_2):
-    """verifying that the full neutron energy gap is correctly calculated"""
-    Delta_n = gp.gap_neutrons_full(test_case_2["k_F_n"])
-    assert np.abs((Delta_n - test_case_2["Delta_n"]) / Delta_n) < TOL
+def test_gap_singlet_neutrons_01(test_case_2):
+    """verifying that the singlet neutron energy gap vanishes at low k_F_n"""
+    Delta_n = gp.gap_singlet_neutrons(
+        test_case_2["k_F_n_left"], gp.neutron_singlet_CCDK
+    )
+    assert Delta_n is test_case_2["Delta_n_left"]
 
 
-def test_gap_protons_full(test_case_2):
-    """verifying that the full proton energy gap is correctly calculated"""
-    Delta_p = gp.gap_protons_full(test_case_2["k_F_p"])
-    assert np.abs((Delta_p - test_case_2["Delta_p"]) / Delta_p) < TOL
+def test_gap_singlet_neutrons_02(test_case_2):
+    """verifying that the singlet neutron energy gap vanishes at high k_F_n"""
+    Delta_n = gp.gap_singlet_neutrons(
+        test_case_2["k_F_n_right"], gp.neutron_singlet_CCDK
+    )
+    assert Delta_n is test_case_2["Delta_n_right"]
 
 
-def test_gap_neutrons_02(test_case_2):
-    """verifying that the neutron energy gap is correctly calculated"""
-    Delta_n = gp.gap_neutrons(test_case_2["k_F_n"])
-    assert np.abs((Delta_n - test_case_2["Delta_n"]) / Delta_n) < TOL
+def test_gap_singlet_protons_01(test_case_3):
+    """verifying that the singlet proton energy gap vanishes at low k_F_n"""
+    Delta_p = gp.gap_singlet_protons(test_case_3["k_F_p_left"], gp.proton_singlet_CCDK)
+    assert Delta_p is test_case_3["Delta_p_left"]
 
 
-def test_gap_protons_02(test_case_2):
-    """verifying that the proton energy gap is correctly calculated"""
-    Delta_p = gp.gap_protons(test_case_2["k_F_p"])
-    assert np.abs((Delta_p - test_case_2["Delta_p"]) / Delta_p) < TOL
+def test_gap_singlet_protons_02(test_case_3):
+    """verifying that the singlet proton energy gap vanishes at high k_F_n"""
+    Delta_p = gp.gap_singlet_protons(test_case_3["k_F_p_right"], gp.proton_singlet_CCDK)
+    assert Delta_p is test_case_3["Delta_p_right"]
 
 
-def test_gap_neutrons_03(test_case_3):
-    """verifying that the neutron energy gap vanishes at high k_F_n"""
-    Delta_n = gp.gap_neutrons(test_case_3["k_F_n"])
-    assert Delta_n is test_case_3["Delta_n"]
+def test_gap_triplet_neutrons_01(test_case_4):
+    """verifying that the triplet neutron energy gap vanishes at low k_F_n"""
+    Delta_n = gp.gap_singlet_neutrons(
+        test_case_4["k_F_n_left"], gp.neutron_triplet_TToa
+    )
+    assert Delta_n is test_case_4["Delta_n_left"]
 
 
-def test_gap_protons_03(test_case_3):
-    """verifying that the proton energy gap vanishes at high k_F_p"""
-    Delta_p = gp.gap_protons(test_case_3["k_F_p"])
-    assert Delta_p is test_case_3["Delta_p"]
+def test_gap_triplet_neutrons_02(test_case_4):
+    """verifying that the triplet neutron energy gap vanishes at high k_F_n"""
+    Delta_n = gp.gap_singlet_neutrons(
+        test_case_4["k_F_n_right"], gp.neutron_triplet_TToa
+    )
+    assert Delta_n is test_case_4["Delta_n_right"]
